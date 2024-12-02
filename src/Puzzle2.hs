1aef927f947526efffddbffa6000cb4f3450de28
Ivan Trepakov <liontiger23@gmail.com>
2024-12-02 23:42:42 +0700

    puzzle2: solve part 2

--- src/Puzzle2.hs
+++ src/Puzzle2.hs
@@ -6,12 +6,12 @@ module Puzzle2
 
 import Util
 import Data.Char ( digitToInt, isDigit )
-import Data.List ( isPrefixOf, elemIndex, sort )
+import Data.List ( isPrefixOf, elemIndex, sort, elemIndices )
 import Data.Maybe ( fromJust )
 
 puzzle2 :: Int -> Solution Int
-puzzle2 1 = length . filter safe . map (diffs . ints)
-puzzle2 2 = undefined
+puzzle2 1 = length . filter safe  . map (diffs . ints)
+puzzle2 2 = length . filter (safe' True) . map ints
 
 ints :: String -> [Int]
 ints = map read . words
@@ -24,3 +24,11 @@ diffs (x:y:xs) = y - x : diffs (y:xs)
 safe :: [Int] -> Bool
 safe xs = (all (> 0) xs || all (< 0) xs) && all ((<= 3) . abs) xs
 
+safe' :: Bool -> [Int] -> Bool
+safe' False xs' = (all (> 0) xs || all (< 0) xs) && all ((<= 3) . abs) xs
+ where xs = diffs xs'
+safe' True xs = safe' False xs || any (safe' False) (drops xs)
+
+drops :: [Int] -> [[Int]]
+drops [] = []
+drops (x:xs) = xs : map (x:) (drops xs)
