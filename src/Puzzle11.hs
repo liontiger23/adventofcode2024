20231248adf9e5d23fb265d1158a570b0b132f28
Ivan Trepakov <liontiger23@gmail.com>
2024-12-12 00:48:50 +0700

    puzzle11: solve part 2 with help from stackoverflow

--- src/Puzzle11.hs
+++ src/Puzzle11.hs
@@ -14,6 +14,7 @@ import Data.Map ((!))
 import Data.Map qualified as M
 import Data.Maybe (isJust)
 import Data.List (nub, groupBy)
+import Control.Monad.State
 
 puzzle11 :: Int -> Solution Integer
 puzzle11 1 = solve1
@@ -23,13 +24,9 @@ solve1 :: Solution Integer
 solve1 = fromIntegral . length . (!! 25) . iterate blink . map read . words . head
 
 solve2 :: Solution Integer
-solve2 = undefined
+solve2 = sum . map (memoizedBlink 75 . read) . words . head
 
 ----------------------------------------
-
-
-----------------------------------------
-
 
 -- >>> blink [125,17]
 -- [253000,1,7]
@@ -49,3 +46,26 @@ blink (x : xs)
  where
   str = show x
   (l, r) = splitAt (length str `div` 2) str
+
+-- Stolen from https://stackoverflow.com/a/44508289
+
+type Memo = State (M.Map (Int, Integer) Integer) Integer
+
+memoizedBlink :: Int -> Integer -> Integer
+memoizedBlink n d = evalState (blink' (n,d)) M.empty
+ where
+  memoo :: ((Int, Integer) -> Memo) -> (Int, Integer) -> Memo
+  memoo f x = gets (M.lookup x) >>= maybe b' return
+   where
+    b' = do
+      b <- f x
+      modify $ M.insert x b
+      return b
+  blink' (0,_) = return 1
+  blink' (n,x)
+    | x == 0 = memoo blink' ((n - 1),1)
+    | even (length str) = (+) <$> memoo blink' ((n - 1),(read l)) <*> memoo blink' ((n - 1),(read r))
+    | otherwise = memoo blink' ((n - 1),(x * 2024))
+   where
+    str = show x
+    (l, r) = splitAt (length str `div` 2) str
