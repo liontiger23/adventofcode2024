f9f0277046c714c923e074be519702dba2c38467
Ivan Trepakov <liontiger23@gmail.com>
2024-12-21 16:51:17 +0700

    puzzle21: solve part 2

--- src/Puzzle21.hs
+++ src/Puzzle21.hs
@@ -33,7 +33,7 @@ solve1 :: Solution Result
 solve1 = sum . map (complexity 3)
 
 solve2 :: Solution Result
-solve2 = undefined
+solve2 = sum . map (complexity 26)
 
 type Result = Integer
 
@@ -51,26 +51,57 @@ type Result = Integer
 -- 24256
 
 complexity :: Int -> [Char] -> Integer
-complexity n cs = read (init cs) * fromIntegral (minimum $ map length $ moveSequence n cs)
+complexity n cs = read (init cs) * moveLength n cs
 
--- >>> minimumBy (comparing length) $ moveSequence 1 "029A"
--- [L,A,U,A,R,U,U,A,D,D,D,A]
--- >>> minimumBy (comparing length) $ moveSequence 2 "029A"
--- [D,L,L,A,R,R,U,A,L,A,R,A,D,A,L,U,A,A,R,A,L,D,A,A,A,R,U,A]
--- >>> minimumBy (comparing length) $ moveSequence 3 "029A"
--- [L,D,A,L,A,A,R,R,U,A,D,A,A,L,U,A,R,A,D,L,L,A,R,R,U,A,D,A,U,A,L,D,A,R,U,A,D,L,L,A,R,U,A,R,A,A,D,A,U,A,D,L,L,A,R,A,R,U,A,A,A,D,A,L,U,A,R,A]
--- >>> minimumBy (comparing length) $ moveSequence 3 "379A"
--- [D,L,L,A,R,R,U,A,D,A,U,A,L,D,A,L,A,A,R,R,U,A,A,D,A,L,U,A,R,A,A,D,A,U,A,L,D,A,R,U,A,A,L,A,R,A,D,L,L,A,R,A,R,U,A,A,A,D,A,L,U,A,R,A]
+-- >>> moveSequence 1 "029A"
+-- [L,A,U,A,U,U,R,A,D,D,D,A]
+-- >>> moveSequence 2 "029A"
+-- [D,L,L,A,R,R,U,A,L,A,R,A,L,A,A,D,R,A,U,A,L,D,A,A,A,U,R,A]
+-- >>> moveSequence 3 "029A"
+-- [L,D,A,L,A,A,R,R,U,A,D,A,A,L,U,A,R,A,D,L,L,A,R,R,U,A,D,A,U,A,D,L,L,A,R,R,U,A,A,L,D,A,R,A,U,A,L,A,R,A,D,L,L,A,R,A,U,R,A,A,A,L,A,D,R,A,U,A]
+-- >>> moveSequence 3 "379A"
+-- [D,L,L,A,R,R,U,A,D,A,U,A,L,D,A,L,A,A,R,R,U,A,A,D,A,L,U,A,R,A,A,D,A,U,A,L,D,A,U,R,A,A,L,A,R,A,D,L,L,A,R,A,U,R,A,A,A,L,A,D,R,A,U,A]
 
-moveSequence :: Int -> [Char] -> [[Move]]
-moveSequence 1 cs = getMoves numericKeymap ('A' : cs)
-moveSequence k cs = concatMap (getMoves directionalKeymap . (A :)) (moveSequence (k - 1) cs)
+moveSequence :: Int -> [Char] -> [Move]
+moveSequence n cs = moveSequence' n
+ where
+  numMap = numericKeymap
+  dirMap = directionalKeymap
+  moveSequence' 1 = getMoves numMap ('A' : cs)
+  moveSequence' k = getMoves dirMap (A : moveSequence' (k - 1))
+
+-- >>> moveLength 1 "029A"
+-- 12
+-- >>> moveLength 2 "029A"
+-- 28
+-- >>> moveLength 3 "029A"
+-- 68
+-- >>> moveLength 3 "379A"
+-- 64
+
+moveLength :: Int -> [Char] -> Integer
+moveLength n cs = evalState (moveLength' n (A : getMoves numericKeymap ('A' : cs))) M.empty
+
+moveLength' :: Int -> [Move] -> State Mem Integer
+moveLength' n [x] = pure 0
+moveLength' n (x : y : xs) = (+) <$> move1Length' n x y <*> moveLength' n (y : xs)
+
+move1Length' :: Int -> Move -> Move -> State Mem Integer
+move1Length' 1 _ _ = pure 1
+move1Length' n x y = do
+  m <- get
+  case M.lookup ((x, y), n) m of
+    Just v  -> pure v
+    Nothing -> do
+      v <- moveLength' (n - 1) $ A : directionalKeymap ! (x, y)
+      modify (M.insert ((x, y), n) v)
+      pure v
 
 ----------------------------------------
 
-getMoves :: Ord a => Keymap a -> [a] -> [[Move]]
-getMoves _ [x] = [[]]
-getMoves m (x : y : xs) = concatMap (\ys -> [ns ++ ys| ns <- m ! (x, y)]) $ getMoves m (y : xs)
+getMoves :: Ord a => Keymap a -> [a] -> [Move]
+getMoves _ [x] = []
+getMoves m (x : y : xs) = m ! (x, y) ++ getMoves m (y : xs)
 
 numericKeymap :: Keymap Char
 numericKeymap = keymap numericKeypad
@@ -93,15 +124,15 @@ directionalKeypad = keypad
   ]
 
 -- >>> getMoves directionalKeymap [A,D,L,A]
--- [[L,D,A,L,A,R,R,U,A],[D,L,A,L,A,R,R,U,A]]
+-- [L,D,A,L,A,R,R,U,A]
 -- >>> getMoves directionalKeymap [A,L,D,A]
--- [[D,L,L,A,R,A,R,U,A],[D,L,L,A,R,A,U,R,A]]
+-- [D,L,L,A,R,A,U,R,A]
 
 -- >>> keymap directionalKeypad
--- fromList [((U,U),[[A]]),((U,D),[[D,A]]),((U,L),[[D,L,A]]),((U,R),[[R,D,A],[D,R,A]]),((U,A),[[R,A]]),((D,U),[[U,A]]),((D,D),[[A]]),((D,L),[[L,A]]),((D,R),[[R,A]]),((D,A),[[R,U,A],[U,R,A]]),((L,U),[[R,U,A]]),((L,D),[[R,A]]),((L,L),[[A]]),((L,R),[[R,R,A]]),((L,A),[[R,R,U,A]]),((R,U),[[L,U,A],[U,L,A]]),((R,D),[[L,A]]),((R,L),[[L,L,A]]),((R,R),[[A]]),((R,A),[[U,A]]),((A,U),[[L,A]]),((A,D),[[L,D,A],[D,L,A]]),((A,L),[[D,L,L,A]]),((A,R),[[D,A]]),((A,A),[[A]])]
+-- fromList [((L,L),[A]),((L,D),[R,A]),((L,U),[R,U,A]),((L,R),[R,R,A]),((L,A),[R,R,U,A]),((D,L),[L,A]),((D,D),[A]),((D,U),[U,A]),((D,R),[R,A]),((D,A),[U,R,A]),((U,L),[D,L,A]),((U,D),[D,A]),((U,U),[A]),((U,R),[D,R,A]),((U,A),[R,A]),((R,L),[L,L,A]),((R,D),[L,A]),((R,U),[L,U,A]),((R,R),[A]),((R,A),[U,A]),((A,L),[D,L,L,A]),((A,D),[L,D,A]),((A,U),[L,A]),((A,R),[D,A]),((A,A),[A])]
 
 keymap :: Ord a => Keypad a -> Keymap a
-keymap m = M.fromList [((a, b), map (++ [A]) $ moves x y) | (x, a) <- M.toList m, (y, b) <- M.toList m]
+keymap m = M.fromList [((a, b), minimum (moves x y) ++ [A]) | (x, a) <- M.toList m, (y, b) <- M.toList m]
  where
   moves :: Coordinate -> Coordinate -> [[Move]]
   moves a@(ax, ay) b@(bx, by)
@@ -138,15 +169,17 @@ type Coordinate = (Int, Int)
 (.-.) :: Coordinate -> Coordinate -> Coordinate
 (a, b) .-. (c, d) = (a - c, b - d)
 
-data Move = U | D | L | R | A
+data Move = L | D | U | R | A
   deriving (Show, Eq, Ord, Enum)
 
 type Keypad a = M.Map Coordinate a
 
-type Keymap a = M.Map (a, a) [[Move]]
+type Keymap a = M.Map (a, a) [Move]
 
 -- >>> keypad [[Just 1, Just 2],[Nothing, Just 0]]
 -- fromList [((0,0),1),((1,0),2),((1,1),0)]
 
 keypad :: [[Maybe a]] -> Keypad a
 keypad vvs = M.fromList [((x,y), v) | (y, vs) <- zip [0..] vvs, (x, Just v) <- zip [0..] vs]
+
+type Mem = M.Map ((Move, Move), Int) Integer
