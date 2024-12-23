fb85024bfb0c12e6ff8334eea4827a2477530d33
Ivan Trepakov <liontiger23@gmail.com>
2024-12-23 12:43:21 +0700

    puzzle23: solve part 1

--- src/Puzzle23.hs
+++ src/Puzzle23.hs
@@ -32,12 +32,61 @@ puzzle23 1 = solve1
 puzzle23 2 = solve2
 
 solve1 :: Solution Result
-solve1 = undefined
+solve1 = S.size . S.filter (any ((== 't') . head) . S.elems) . subnets 3 . parseNetwork
 
 solve2 :: Solution Result
 solve2 = undefined
 
-type Result = Integer
+type Result = Int
 
 ----------------------------------------
 
+-- >>> subnets 1 $ parseNetwork ["tc-th","ab-tc","th-ab","th-gy"]
+-- fromList [fromList ["ab"],fromList ["gy"],fromList ["tc"],fromList ["th"]]
+--
+-- >>> subnets 2 $ parseNetwork ["tc-th","ab-tc","th-ab","th-gy"]
+-- fromList [fromList ["ab","tc"],fromList ["ab","th"],fromList ["gy","th"],fromList ["tc","th"]]
+--
+-- >>> subnets 3 $ parseNetwork ["tc-th","ab-tc","th-ab","th-gy"]
+-- fromList [fromList ["ab","tc","th"]]
+--
+-- >>> neighbours (parseNetwork ["tc-th","ab-tc","th-ab","th-gy"]) (S.fromList ["ab"])
+-- ["tc","th"]
+--
+-- >>> parseNetwork ["tc-th","ab-tc","th-ab","th-gy"]
+-- fromList [("ab",fromList ["tc","th"]),("gy",fromList ["th"]),("tc",fromList ["ab","th"]),("th",fromList ["ab","gy","tc"])]
+
+subnets :: Int -> Network -> S.Set (S.Set Node)
+subnets 1 m = S.map S.singleton $ M.keysSet m
+subnets k m = S.fromList $ concatMap expand $ S.elems $ subnets (k - 1) m
+ where
+  expand :: S.Set Node -> [S.Set Node]
+  expand s = map (`S.insert` s) $ neighbours m s
+
+
+neighbours :: Network -> S.Set Node -> [Node]
+neighbours m s = S.elems $ foldr (S.intersection . (m !)) (M.keysSet m) (S.elems s)
+
+
+parseNetwork :: [String] -> Network
+parseNetwork input = execState (mapM (fill . parseConnection) input) M.empty
+ where
+  fill :: Connection -> State Network ()
+  fill (x, y) = modify (M.insertWith S.union x (S.singleton y) . M.insertWith S.union y (S.singleton x))
+
+-- >>> parseConnection "tc-th"
+-- ("tc","th")
+
+parseConnection :: String -> Connection
+parseConnection (a1 : a2 : _ : b1 : b2 : _) = ([a1, a2], [b1, b2])
+
+----------------------------------------
+
+type Node = String
+
+type Connection = (Node, Node)
+
+type Network = M.Map Node (S.Set Node)
+
+connected :: Network -> Node -> Node -> Bool
+connected net x y = maybe False (S.member y) $ M.lookup x net
